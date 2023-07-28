3c86eae0a8ac8830649184e691cee13b9eb8b91f
leo1367 <leonardoherculano136@gmail.com>
2023-07-28 19:32:21 -0300

    teste

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let itens = document.getElementById('burge')
+
+function menuOnClick() {
+    
+    if (itens.style.display == 'block') {
+
+        itens.style.display = 'none'
+    } else {
+        
+        itens.style.display = 'block'
+        
+    }
+
+}
